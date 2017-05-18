37941ec817b1edf5b31b4c4bd96d1442bc4e6634
vagrant <m0naaa0u@gmail.com>
2017-05-18 18:13:53 +0100

    a

--- app_main.rb
+++ app_main.rb
@@ -29,7 +29,7 @@ post '/callback' do
       when Line::Bot::Event::MessageType::Text
         message = {
           type: 'text',
-          text: event.message['text']
+          text: event.message['text']+'♡'
         }
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
