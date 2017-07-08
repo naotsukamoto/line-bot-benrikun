514a5f9e6433fb678de2fb16683d987ea838c1d2
vagrant <m0naaa0u@gmail.com>
2017-07-08 10:46:19 +0100

    heroku deploy test

--- task.rb
+++ task.rb
@@ -1,1 +1,16 @@
-puts "done"
+get '/push' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  message_1 = {
+    type: 'text',
+    text: 'aaa'
+  }
+  client.reply_message(event['replyToken'], message_1)
+
+  puts "done"
+end
